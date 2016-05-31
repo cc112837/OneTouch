3584244c3cabd3ccf87d4ef527124d48cb845a09
cc112837@163.com <cc112837@163.com>
2016-05-31 11:27:10 +0800

    好友详情界面

--- app/src/main/java/com/wzy/mhealth/activities/FriendDetailActivity.java
+++ app/src/main/java/com/wzy/mhealth/activities/FriendDetailActivity.java
@@ -2,7 +2,6 @@ package com.wzy.mhealth.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
