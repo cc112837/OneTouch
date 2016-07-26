46ed0d08bd9b97e044830474d6a8ad8d5f4836ec
cc112837@163.com <cc112837@163.com>
2016-07-26 13:41:31 +0800

    添加隐私服务页面

--- app/src/main/java/com/wzy/mhealth/activities/AboutActivity.java
+++ app/src/main/java/com/wzy/mhealth/activities/AboutActivity.java
@@ -1,18 +1,31 @@
 package com.wzy.mhealth.activities;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.wzy.mhealth.R;
+import com.wzy.mhealth.utils.PackageUtils;
 
 public class AboutActivity extends Activity {
-
+private TextView tv_version,tv_service;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+        tv_service=(TextView) findViewById(R.id.tv_service);
+        tv_service.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(AboutActivity.this,ServicePri.class);
+                startActivity(intent);
+            }
+        });
+        tv_version=(TextView) findViewById(R.id.tv_version);
+        tv_version.setText("版本："+ PackageUtils.getPackageVersion(this));
         ImageView leftBtn=(ImageView) findViewById(R.id.leftBtn);
         leftBtn.setOnClickListener(new View.OnClickListener() {
             @Override
---
+++ app/src/main/java/com/wzy/mhealth/activities/ServicePri.java
@@ -0,0 +1,36 @@
+package com.wzy.mhealth.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.widget.ImageView;
+
+import com.wzy.mhealth.R;
+
+public class ServicePri extends Activity {
+    private ImageView leftBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_service_pri);
+        WebView wv_show=(WebView)findViewById(R.id.wv_show);
+        WebSettings webSettings = wv_show.getSettings();
+        webSettings.setSupportZoom(true);
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setJavaScriptCanOpenWindowsAutomatically(true);
+        webSettings.setBuiltInZoomControls(true);//support zoom
+        webSettings.setUseWideViewPort(true);// 这个很关键
+        webSettings.setLoadWithOverviewMode(true);
+        wv_show.loadUrl("http://www.xa-hengli.com/page/QT/99.php");//// TODO: 2016/7/25 (服务协议)
+        leftBtn=(ImageView) findViewById(R.id.leftBtn);
+        leftBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+}
