8ae28902970c68cda0c90ec0f5ddcb358fa15998
cc <cc112837@163.com>
2016-08-15 15:46:16 +0800

    判断资讯是否有详情页

--- app/src/main/java/com/wzy/mhealth/activities/NewsDetailActivity.java
+++ app/src/main/java/com/wzy/mhealth/activities/NewsDetailActivity.java
@@ -13,6 +13,7 @@ import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.wzy.mhealth.R;
 import com.wzy.mhealth.model.NewDetail;
@@ -27,7 +28,7 @@ public class NewsDetailActivity extends BaActivity {
             switch (msg.what) {
                 case 22:
                     NewDetail newDetail = (NewDetail) msg.obj;
-                    WebSettings webSettings= wv_dis.getSettings();
+                    WebSettings webSettings = wv_dis.getSettings();
                     webSettings.setUseWideViewPort(true);
                     webSettings.setTextZoom(120);
                     wv_dis.loadData(newDetail.getData().getContent().getArticle().getHtml(), "text/html; charset=utf-8", "utf-8");
@@ -41,17 +42,23 @@ public class NewsDetailActivity extends BaActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news_detail);
         Intent intent = getIntent();
-        String contentid = intent.getStringExtra("content");
-        wv_dis=(WebView) findViewById(R.id.wv_dis);
-        String url = "http://api.m.vodjk.com/v1/article?contentid=" + contentid + "&height=667.0&ip=192.168.1.107&modules=content%3A1&sign=983a172c96d973a4a528c02844bea991&time=1468463373&token=3&type=android&width=375.0";
-        MyHttpUtils.handData(handler, 22, url, null);
-        leftBtn = (ImageView) findViewById(R.id.leftBtn);
-        leftBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
+        if (intent != null) {
+            String contentid = intent.getStringExtra("content");
+            if (contentid == null) {
+                Toast.makeText(NewsDetailActivity.this, "没有具体内容", Toast.LENGTH_LONG).show();
+            } else {
+                wv_dis = (WebView) findViewById(R.id.wv_dis);
+                String url = "http://api.m.vodjk.com/v1/article?contentid=" + contentid + "&height=667.0&ip=192.168.1.107&modules=content%3A1&sign=983a172c96d973a4a528c02844bea991&time=1468463373&token=3&type=android&width=375.0";
+                MyHttpUtils.handData(handler, 22, url, null);
             }
-        });
+            leftBtn = (ImageView) findViewById(R.id.leftBtn);
+            leftBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    finish();
+                }
+            });
+        }
     }
 
 }
