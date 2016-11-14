c892df0d9b0baf062389d0831cc45986ec67971f
cc <cc112837@163.com>
2016-11-14 08:54:48 +0800

    修改商城页面

--- app/src/main/java/com/wzy/mhealth/fragments/ShopFragment.java
+++ app/src/main/java/com/wzy/mhealth/fragments/ShopFragment.java
@@ -6,20 +6,38 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.wzy.mhealth.R;
 
-/**
- * A simple {@link Fragment} subclass.
- */
-public class ShopFragment extends Fragment {
+public class ShopFragment extends Fragment implements View.OnClickListener {
+    private ImageView leftBtn, rightBtn;
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_shop, container, false);
+        View v = inflater.inflate(R.layout.fragment_shop, container, false);
+        init(v);
+        return v;
+    }
+
+    private void init(View v) {
+        leftBtn = (ImageView) v.findViewById(R.id.leftBtn);
+        rightBtn = (ImageView) v.findViewById(R.id.rightBtn);
     }
 
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.leftBtn:
+                Toast.makeText(getActivity(), "您点击了订单", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.rightBtn:
+                Toast.makeText(getActivity(), "您点击了购物车", Toast.LENGTH_LONG).show();
+                break;
+        }
+    }
 }
