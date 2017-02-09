e74f0286c054562ba815e707be7d5a710ce46173
cc <cc112837@163.com>
2017-02-09 10:51:32 +0800

    新添预约挂号页面

---
+++ app/src/main/java/com/wzy/mhealth/activities/OrderDoctorActivity.java
@@ -0,0 +1,15 @@
+package com.wzy.mhealth.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.wzy.mhealth.R;
+
+public class OrderDoctorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_doctor);
+    }
+}
